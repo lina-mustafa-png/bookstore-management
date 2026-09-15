eb76b88c19059e0c71e49e9e14e1da0d9bb9484e
lina-mustafa-png <lina9.mustafa90@gmail.com>
2026-09-15 15:44:26 -0400

    Update OwnerStartPanel.java

--- src/bookstore/OwnerStartPanel.java
+++ src/bookstore/OwnerStartPanel.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JPanel.java to edit this template
- */
 package bookstore;
 
-/**
- *
- * @author anant
- */
 public class OwnerStartPanel extends javax.swing.JPanel {
 
     /**
