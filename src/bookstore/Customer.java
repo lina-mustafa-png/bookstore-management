b04c733b2beddf81f6e4e20bc249b483448753e1
lina-mustafa-png <lina9.mustafa90@gmail.com>
2026-09-15 15:43:16 -0400

    Update Customer.java

--- src/bookstore/Customer.java
+++ src/bookstore/Customer.java
@@ -1,15 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package bookstore;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author Darla
- */
 //the Customer extends the User
 public class Customer extends User {
     
