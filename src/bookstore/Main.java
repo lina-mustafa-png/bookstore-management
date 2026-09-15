a28aaa1da30d7375e4271b41570deec61106fe90
lina-mustafa-png <lina9.mustafa90@gmail.com>
2026-09-15 15:43:59 -0400

    Update Main.java

--- src/bookstore/Main.java
+++ src/bookstore/Main.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package bookstore;
 
-/**
- *
- * @author lina9
- */
 public class Main {
     public static void main(String[] args){
         BookStore bookstore = new BookStore(); // making the bookstore
