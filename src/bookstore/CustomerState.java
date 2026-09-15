01ecaa136fd811b7bb4fd5b2acec1d1d967d6d38
lina-mustafa-png <lina9.mustafa90@gmail.com>
2026-09-15 15:43:34 -0400

    Update CustomerState.java

--- src/bookstore/CustomerState.java
+++ src/bookstore/CustomerState.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package bookstore;
 
-/**
- *
- * @author Darla
- */
 //uses an interface for the State Design Pattern
 public interface CustomerState {
     String getStatus(); // returns status name (Gold or Silver)
