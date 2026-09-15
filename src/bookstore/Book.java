40117738daa28152bb9d843478c82da91cd5f991
lina-mustafa-png <lina9.mustafa90@gmail.com>
2026-09-15 15:42:39 -0400

    Update Book.java

--- src/bookstore/Book.java
+++ src/bookstore/Book.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package bookstore;
 
-/**
- *
- * @author Darla
- */
 //represents a book that has a name and a price
 public class Book {
     private double price;
