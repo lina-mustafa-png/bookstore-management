21b690ddece611ea3c521288e9e2e4b90bdffecc
lina-mustafa-png <lina9.mustafa90@gmail.com>
2026-09-15 15:42:57 -0400

    Update BookStoreGUI.java

--- src/bookstore/BookStoreGUI.java
+++ src/bookstore/BookStoreGUI.java
@@ -1,13 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package bookstore;
-
-/**
- *
- * @author lina9
- */
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
